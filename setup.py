dbca3f260458b0988b011cb7f0155fb82956b114
gangadhar <s210267@rguktsklm.ac.in>
2025-08-29 20:46:56 +0530

    Data ingestion

--- setup.py
+++ setup.py
@@ -2,13 +2,10 @@ from setuptools import find_packages, setup
 from typing import List
 
 def get_requirements(file_path: str) -> List[str]:
-    HYPEN_E_DOT = "-e ."
     requirements = []
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
         requirements = [req.strip() for req in requirements]
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
     return requirements
 
 setup(
